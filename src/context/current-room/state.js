65bd4dbccaff800432a8ade8d1576ed19e351682
Amir Honarvar <mezz@Amirs-MacBook-Pro.local>
2022-07-17 16:24:02 +0430

    bug fixed

--- src/context/current-room/state.js
+++ src/context/current-room/state.js
@@ -1,102 +1,104 @@
-import { useState } from 'react'
-import CurrentRoomContext from './context.js'
+import { useState } from "react";
+import CurrentRoomContext from "./context.js";
 
 const initial = {
-    messages: [],
-    users_ids: [],
-    room_name: '',
-    room_id: '',
-    awaitFetchMessages: true,
-    awaitFetchRoom: true,
-    areMessagesFinished: false,
-}
+	messages: [],
+	users_ids: [],
+	room_name: "",
+	room_id: "",
+	awaitFetchMessages: true,
+	awaitFetchRoom: true,
+	areMessagesFinished: false,
+};
 
-const CurrentRoomState = (props) => {
+const CurrentRoomState = props => {
+	const [currentRoomState, setCurrentRoomState] = useState(initial);
 
-    const [ currentRoomState, setCurrentRoomState ] = useState(initial)
+	const setCurrentRoom = room => {
+		setCurrentRoomState({
+			...currentRoomState,
+			...room,
+			awaitFetchRoom: false,
+		});
+	};
 
-    const setCurrentRoom = (room) => {
-        setCurrentRoomState({
-            ...currentRoomState,
-            ...room,
-            awaitFetchRoom: false,
-        })
-    }
+	const setMessages = (messages, isFinished, initial = true) => {
+		setCurrentRoomState(state => {
+			return {
+				...state,
+				messages: initial ? [...messages] : [...messages, ...state.messages],
+				awaitFetchMessages: false,
+				areMessagesFinished: isFinished,
+			};
+		});
+	};
 
-    const setMessages = (messages, isFinished, initial=true) => {
-        setCurrentRoomState(state => {
-            return {
-                ...state,
-                messages: initial ? [ ...messages ] : [ ...messages, ...state.messages ],
-                awaitFetchMessages: false,
-                areMessagesFinished: isFinished
-            }
-        })
-    }
+	const addNewMessage = message => {
+		setCurrentRoomState(state => {
+			return {
+				...state,
+				messages: [
+					...state.messages,
+					{
+						message_id: message.message_id,
+						created_at: message.created_at,
+						message_text: message.message_text,
+						sender: {
+							user_id: message.sender_id,
+							profile_picture: message.profile_picture,
+							username: message.username,
+						},
+					},
+				],
+			};
+		});
+	};
 
-    const addNewMessage = (message) => {
-        setCurrentRoomState(state => {
-            return {
-                ...state,
-                messages: [...state.messages, {
-                    message_id: message.message_id,
-                    created_at: message.created_at,
-                    message_text: message.message_text,
-                    sender: {
-                        user_id: message.sender_id,
-                        profile_picture: message.profile_picture,
-                        username: message.username
-                    }
-                }]
-            }
-        })
-    }
+	const editMessage = (messageId, messageText) => {
+		console.log(messageId, messageText);
+		setCurrentRoomState(state => {
+			return {
+				...state,
+				messages: state.messages.map(mappedMessage => {
+					if (mappedMessage.message_id === messageId) {
+						mappedMessage.message_text = messageText;
+					}
+					return mappedMessage;
+				}),
+			};
+		});
+	};
 
-    const editMessage = (messageId, messageText) => {
-        console.log(messageId, messageText)
-        setCurrentRoomState(state => {
-            return {
-                ...state,
-                messages: state.messages.map(mappedMessage => {
-                    if(mappedMessage.message_id === messageId) {
-                        mappedMessage.message_text = messageText
-                    }
-                    return mappedMessage
-                })
-            }
-        })
-    }
+	const removeMessage = messageId => {
+		setCurrentRoomState(state => {
+			return {
+				...state,
+				messages: state.messages.filter(message => {
+					return message.message_id !== messageId;
+				}),
+			};
+		});
+	};
 
-    const removeMessage = (messageId) => {
-        setCurrentRoomState(state => {
-            return {
-                ...state,
-                messages: state.messages.filter(message => {
-                    return message.message_id !== messageId
-                })
-            }
-        })
-    }
+	const submitCurrentRoom = () => {
+		setCurrentRoomState({ ...currentRoomState, submitted: true });
+	};
 
-    const submitCurrentRoom = () => {
-        setCurrentRoomState({...currentRoomState, submitted: true})
-    }
+	return (
+		<CurrentRoomContext.Provider
+			value={{
+				currentRoom: currentRoomState,
+				setCurrentRoom: setCurrentRoom,
+				submitCurrentRoom: submitCurrentRoom,
+				addNewMessage: addNewMessage,
+				setMessages: setMessages,
+				removeMessage: removeMessage,
+				editMessage: editMessage,
+			}}
+		>
+			{props.children}
+		</CurrentRoomContext.Provider>
+	);
+};
 
-    return (
-        <CurrentRoomContext.Provider value={{
-            currentRoom: currentRoomState,
-            setCurrentRoom: setCurrentRoom,
-            submitCurrentRoom: submitCurrentRoom,
-            addNewMessage: addNewMessage,
-            setMessages: setMessages,
-            removeMessage: removeMessage,
-            editMessage: editMessage
-        }}>
-
-            {props.children}
-
-        </CurrentRoomContext.Provider>
-    )
-}
-
-export default CurrentRoomState
+export default CurrentRoomState;
--- src/pages/Home/ChatRoom.jsx
+++ src/pages/Home/ChatRoom.jsx
@@ -1,149 +1,174 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState, useRef } from 'react'
-import { useSocket, useAuth, useCurrentRoom } from '../../context/index.js'
-import roomsAPI from '../../api/rooms.js'
-import Message from './Message.jsx'
-import RoomInfo from './RoomInfo.jsx'
+import { useEffect, useState, useRef } from "react";
+import { useSocket, useAuth, useCurrentRoom } from "../../context/index.js";
+import roomsAPI from "../../api/rooms.js";
+import Message from "./Message.jsx";
+import RoomInfo from "./RoomInfo.jsx";
 
 const ChatRoom = () => {
+	const {
+		currentRoom,
+		addNewMessage,
+		setMessages,
+		removeMessage,
+		editMessage,
+	} = useCurrentRoom();
+	const { auth } = useAuth();
+	const {
+		socketState: { socket },
+	} = useSocket();
 
-    const { currentRoom, addNewMessage, setMessages, removeMessage, editMessage } = useCurrentRoom()
-    const { auth } = useAuth()
-    const { socketState: {socket} } = useSocket()
+	const firstMessage = useRef(null);
+	const scrollSpan = useRef(null);
+	const observer = useRef(null);
+	const newMessage = useRef(false);
 
-    const firstMessage = useRef(null)
-    const scrollSpan = useRef(null)
-    const observer = useRef(null)
-    const newMessage = useRef(false)
+	const observerCallback = entries => {
+		entries.forEach(entry => {
+			if (!entry.target.id === "firstMessage" || !entry.isIntersecting) return;
+			roomsAPI
+				.getRoomMessages(
+					currentRoom.room_id,
+					auth.token,
+					currentRoom.messages.length
+				)
+				.then(({ data }) => {
+					const messages = data.messages.reverse();
+					setMessages(messages, data.isFinished, false);
+				})
+				.catch(error => console.log(error));
+		});
+	};
 
-    const observerCallback = (entries) => {
-        entries.forEach(entry => {
-            if(!entry.target.id === 'firstMessage' || !entry.isIntersecting) return
-            roomsAPI.getRoomMessages(currentRoom.room_id, auth.token, currentRoom.messages.length)
-            .then(({ data }) => {setMessages(data.messages, data.isFinished, false)})
-            .catch(error => console.log(error))
-        });
-    }
+	const [messageText, setMessageText] = useState("");
+	const [showRoomInfo, setShowRoomInfo] = useState(false);
 
-    const [messageText, setMessageText] = useState('')
-    const [showRoomInfo, setShowRoomInfo] = useState(false)
+	const sendMessage = async e => {
+		e.preventDefault();
+		setMessageText("");
 
-    const sendMessage = async (e) => {
-        e.preventDefault()
-        setMessageText('')
+		socket.emit("chat-room/new-message", {
+			roomId: currentRoom.room_id,
+			messageText,
+			senderId: auth.userId,
+		});
+	};
 
-        socket.emit("chat-room/new-message", ({
-            roomId: currentRoom.room_id,
-            messageText,
-            senderId: auth.userId
-        }))
-    }
+	useEffect(() => {
+		if (showRoomInfo) setShowRoomInfo(false);
 
-    useEffect(() => {
+		roomsAPI
+			.getRoomMessages(currentRoom.room_id, auth.token)
+			.then(({ data }) => {
+				console.log(data.messages);
+				newMessage.current = true;
+				setMessages(data.messages, data.isFinished);
+			})
+			.catch(error => console.log(error));
 
-        if(showRoomInfo) setShowRoomInfo(false)
+		socket.on("chat-room/new-message", message => {
+			newMessage.current = true;
+			addNewMessage(message);
+		});
+		socket.on("chat-room/message-deleted", messageId => {
+			removeMessage(messageId);
+		});
+		socket.on("chat-room/message-edited", editedMessage => {
+			editMessage(editedMessage.message_id, editedMessage.message_text);
+		});
 
-        roomsAPI.getRoomMessages(currentRoom.room_id, auth.token)
-         .then(({data}) => {
-            newMessage.current = true
-            setMessages(data.messages, data.isFinished)
-        })
-         .catch(error => console.log(error))
+		return () => {
+			socket.removeListener("chat-room/new-message");
+			socket.removeListener("chat-room/message-edited");
+			socket.removeListener("chat-room/message-deleted");
+		};
+	}, [currentRoom.room_id]);
 
-        socket.on("chat-room/new-message", (message) => {
-            newMessage.current = true
-            addNewMessage(message)
-        })
-        socket.on("chat-room/message-deleted", (messageId) => {
-            removeMessage(messageId)
-        })
-        socket.on("chat-room/message-edited", (editedMessage) => {
-            editMessage(editedMessage.message_id, editedMessage.message_text)
-        })
+	useEffect(() => {
+		if (currentRoom.awaitFetchMessages) return;
 
-        return () => {
-            socket.removeListener('chat-room/new-message')
-            socket.removeListener('chat-room/message-edited')
-            socket.removeListener('chat-room/message-deleted')
-        }
-    }, [currentRoom.room_id])
+		if (newMessage.current) {
+			scrollSpan.current.scrollIntoView({ behavior: "smooth" });
+			newMessage.current = false;
+		}
 
+		if (currentRoom.areMessagesFinished) {
+			if (observer.current) observer.current.disconnect();
+			return;
+		}
 
+		setTimeout(() => {
+			observer.current = new IntersectionObserver(observerCallback, {
+				rootMargin: "-100px 0px 0px -100px",
+			});
+			observer.current.observe(firstMessage.current);
+		}, 1000);
 
-    useEffect(() => {
-        if(currentRoom.awaitFetchMessages) return
+		return () => {
+			if (observer.current) observer.current.disconnect();
+		};
+	}, [currentRoom.messages]);
 
-        if(newMessage.current) {
-            scrollSpan.current.scrollIntoView({ behavior: "smooth" })
-            newMessage.current = false
-        }
-        
-        if(currentRoom.areMessagesFinished) {
-            if(observer.current) observer.current.disconnect()
-            return
-        }
-        
-        setTimeout(() => {
-            observer.current = new IntersectionObserver(observerCallback, {rootMargin: '-100px 0px 0px -100px'})
-            observer.current.observe(firstMessage.current)
-        }, 1000)
+	return (
+		<section className='chat-screen'>
+			{showRoomInfo && (
+				<RoomInfo
+					removeComponent={() => setShowRoomInfo(false)}
+					room={currentRoom}
+				/>
+			)}
 
-        return () => {
-            if(observer.current) observer.current.disconnect()
-        }
+			<header className='header'>
+				<h3 className='room-name'>{currentRoom.room_name}</h3>
+				<button
+					onClick={() => setShowRoomInfo(true)}
+					className='room-info not-button'
+				>
+					room info
+				</button>
+			</header>
+			<div className='main-container'>
+				{currentRoom.awaitFetchMessages ? (
+					<h3 className='no-messages'>Loading...</h3>
+				) : currentRoom.messages.length > 0 ? (
+					currentRoom.messages.map((message, i) => {
+						return (
+							<div
+								ref={i === 0 ? firstMessage : undefined}
+								key={message.message_id}
+								itsme={
+									message.sender.user_id === auth.userId ? "true" : "false"
+								}
+								className='message-container'
+								id={message.i === 0 ? "firstMessage" : undefined}
+							>
+								<Message message={message} />
+							</div>
+						);
+					})
+				) : (
+					<h3 className='no-messages'>There is no messages</h3>
+				)}
 
-    }, [currentRoom.messages])
+				<span ref={scrollSpan} style={{ display: "inline" }}></span>
+			</div>
+			<article className='footer'>
+				<form onSubmit={sendMessage} action=''>
+					<textarea
+						required
+						value={messageText}
+						onChange={e => setMessageText(e.target.value)}
+						placeholder='write something'
+						type='text'
+					/>
 
-    return (
+					<button disabled={messageText ? false : true} type='submit'>
+						send
+					</button>
+				</form>
+			</article>
+		</section>
+	);
+};
 
-            <section className="chat-screen">
-
-                {showRoomInfo &&
-                    <RoomInfo
-                     removeComponent={() => setShowRoomInfo(false)}
-                     room={currentRoom}
-                     />}
-
-                <header className="header">
-                    <h3 className="room-name">{currentRoom.room_name}</h3>
-                    <button onClick={() => setShowRoomInfo(true)} className="room-info not-button">room info</button>
-                </header>
-                <div className="main-container">
-                    {currentRoom.awaitFetchMessages 
-                        ? <h3 className="no-messages">Loading...</h3>
-                        : currentRoom.messages.length > 0 
-                            ?  currentRoom.messages.map((message, i) => {
-                                return ( 
-                                    <div
-                                     ref={i === 0 ? firstMessage : undefined}
-                                     key={message.message_id}
-                                     itsme={message.sender.user_id === auth.userId ? 'true' : 'false'}
-                                     className="message-container"
-                                     id={message.i === 0 ? 'firstMessage' : undefined} >
-
-                                        <Message message={message}/>
-                                    </div>
-                                    )
-                                })
-                            :   <h3 className="no-messages">There is no messages</h3>
-                    }
-
-                    <span ref={scrollSpan} style={{display: 'inline'}}></span>
-                </div>
-                <article className="footer">
-                    <form onSubmit={sendMessage} action="">
-                        <textarea
-                         required
-                         value={messageText}
-                         onChange={(e) => setMessageText(e.target.value)}
-                         placeholder="write something"
-                         type="text" />
-                         
-                        <button disabled={messageText ? false : true} type="submit">send</button>
-                    </form>
-                </article>
-            </section>
-    )
-}
-
-export default ChatRoom 
+export default ChatRoom;
